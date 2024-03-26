d482f051df8e82f4b57f36d7e39c70d37b706bf3
369Dawi369 <hockeygamer68@yahoo.com>
2024-03-27 00:15:37 +0100

    Project setup

--- src/main.py
+++ src/main.py
@@ -1,40 +1,54 @@
+import os
+import shutil
 from file_classifier import classify_file
 from organizer import organize_files
+from dataclasses import dataclass
 
-#TODO check on every downloads folder update/1h time period
+
+# TODO check on every downloads folder update/1h time period
+
+
+@dataclass
+class FileMetadata:
+	name: str
+	size: int
+	creation_date: str
+	file_type: str
+
 
 def main():
-    # Assume we get a list of files from the Downloads folder
-    files_to_organize = ["some_file.pdf", "another_file.mp4", ...]
-    classified_files = classify_file(files_to_organize)
-    organize_files(classified_files)
+	# SETUP
+	home_directory = os.path.expanduser('~')
+	downloads_path = os.path.join(home_directory, 'Downloads')
+	dynamic_file_array: [FileMetadata] = []
+
 
 if __name__ == "__main__":
-    main()
+	main()
 
 """
 Downloads/
-	To_Review/
-		Misc/
-	Programming/
-		Python/
-		Rust/
-		Go/
-		Mojo/
-		C/
-		C++/
-	Documents/
-		PDFs/
-		Word Documents/
-		Spreadsheets/
-	Media/
-		Images/
-		Videos/
-		Music/
-	Software/
-		Executables/
-		Installation Packages/
-	Archives/
-		ZIP/
-		RAR/
+    To_Review/
+        Misc/
+    Programming/
+        Python/
+        Rust/
+        Go/
+        Mojo/
+        C/
+        C++/
+    Documents/
+        PDFs/
+        Word Documents/
+        Spreadsheets/
+    Media/
+        Images/
+        Videos/
+        Music/
+    Software/
+        Executables/
+        Installation Packages/
+    Archives/
+        ZIP/
+        RAR/
 """
