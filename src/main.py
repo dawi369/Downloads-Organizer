69b4884a7a6c3c9d3eeab5e0ff45d760254cbcea
369Dawi369 <hockeygamer68@yahoo.com>
2024-03-27 01:19:39 +0100

    Added tests for TDD approach

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ def main():
 	"""Setup"""
 
 	DownloadsFolder = DownloadsFolderClass()
+	my_file = FileMetadata("text.txt", 1000, "21-03-24 11:11", "txt")
+	DownloadsFolder.add_file(my_file)
 	DownloadsFolder.list_files()
 
 
--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,23 @@
 import unittest
-from src.main import *
-from src.Classes import *
-from src.file_classifier import *
-from src.organizer import *
+
+from ..src.Classes import *
+
 
 
 
 
 class TestAddFunction(unittest.TestCase):
 
-	def test(self):
+	def test_download_path(self):
 		downloads_path = os.path.join(os.path.expanduser('~'), 'Downloads')
 		DownloadsFolder = DownloadsFolderClass()
-		self.assertEquals(downloads_path, DownloadsFolder.downloads_path)
+		self.assertEqual(downloads_path, DownloadsFolder.downloads_path)
+
+	def test_adding_file(self):
+		DownloadsFolder = DownloadsFolderClass()
+		my_file = FileMetadata("text.exe", 1000, "21-03-24 11:11", "txt")
+		DownloadsFolder.add_file(my_file)
+		self.assertEqual(DownloadsFolder.files[0], my_file)
 
 if __name__ == '__main__':
 	unittest.main()
